67ac117cab420a448d49e29c83b6851dd2ffbd4d
Stefan <git@anotherwayout.de>
2021-12-06 15:46:00 +0100

    Day 5

---
+++ AoC05/AoC05/Entry.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace AoC05
+{
+    public class Entry
+    {
+        private static readonly Regex _inputFormat = new Regex(@"^(\d+),(\d+) -> (\d+),(\d+)$");
+
+        private readonly Point _start;
+        private readonly Point _end;
+        
+        public Entry(string line)
+        {
+            var match = _inputFormat.Match(line);
+            if (!match.Success)
+            {
+                throw new ArgumentException("Line can't be read!");
+            }
+
+            _start = new Point(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
+            _end = new Point(int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value));
+        }
+
+        public bool IsHorizontal => _start.Y == _end.Y;
+        public bool IsVertical => _start.X == _end.X;
+        public bool IsDiagonal => Math.Abs(_start.X - _end.X) == Math.Abs(_start.Y - _end.Y);
+
+        public IEnumerable<Point> getPointsOnLine()
+        {
+            if (IsHorizontal)
+            {
+                var xStart = Math.Min(_start.X, _end.X);
+                var xEnd = Math.Max(_start.X, _end.X);
+                for (var x = xStart; x <= xEnd; x++)
+                {
+                    yield return new Point(x, _start.Y);
+                }
+                yield break;
+            }
+            
+            if (IsVertical)
+            {
+                var yStart = Math.Min(_start.Y, _end.Y);
+                var yEnd = Math.Max(_start.Y, _end.Y);
+                for (var y = yStart; y <= yEnd; y++)
+                {
+                    yield return new Point(_start.X, y);
+                }
+                yield break;
+            }
+            
+            if (IsDiagonal)
+            {
+                var point = _start;
+                while (point != _end)
+                {
+                    yield return point;
+                    point = new Point
+                    {
+                        X = point.X + (_end.X > _start.X ? 1 : -1),
+                        Y = point.Y + (_end.Y > _start.Y ? 1 : -1)
+                    };
+                }
+                yield return _end;
+                yield break;
+            }
+
+            throw new DataMisalignedException($"Invalid Data: {_start} -> {_end}");
+        }
+    }
+}
---
+++ AoC05/AoC05/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace AoC05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = "Input.txt";
+            task1(filename);
+            task2(filename);
+        }
+
+        private static void task1(string filename)
+        {
+            Console.WriteLine("Task 1:");
+            Console.WriteLine("=======");
+
+            var entries = readDataFromFile(filename);
+            var points = new Dictionary<Point, int>();
+            foreach (var point in entries
+                .Where(entry => entry.IsHorizontal || entry.IsVertical)
+                .SelectMany(entry => entry.getPointsOnLine()))
+            {
+                if (!points.ContainsKey(point))
+                {
+                    points[point] = 1;
+                }
+                else
+                {
+                    points[point]++;
+                }
+            }
+
+            var crossingPoints = points.Count(point => point.Value > 1);
+            Console.WriteLine($"{crossingPoints} crossing points");
+            
+            Console.WriteLine();
+        }
+
+        private static void task2(string filename)
+        {
+            Console.WriteLine("Task 2:");
+            Console.WriteLine("=======");
+
+            var entries = readDataFromFile(filename);
+            var points = new Dictionary<Point, int>();
+            foreach (var point in entries
+                .SelectMany(entry => entry.getPointsOnLine()))
+            {
+                if (!points.ContainsKey(point))
+                {
+                    points[point] = 1;
+                }
+                else
+                {
+                    points[point]++;
+                }
+            }
+
+            var crossingPoints = points.Count(point => point.Value > 1);
+            Console.WriteLine($"{crossingPoints} crossing points");
+
+            Console.WriteLine();
+        }
+
+        private static IEnumerable<Entry> readDataFromFile(string fileName)
+        {
+            var inputFile = new FileInfo(fileName);
+            using var fileStream = inputFile.OpenRead();
+            using var fileReader = new StreamReader(fileStream);
+            while (!fileReader.EndOfStream)
+            {
+                yield return new Entry(fileReader.ReadLine());
+            }
+        }
+    }
+}
