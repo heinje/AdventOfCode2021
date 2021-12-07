51641f8cd37050baaa7e37fea6bc26a374a94ff2
Stefan <git@anotherwayout.de>
2021-12-07 10:52:21 +0100

    Day 6

---
+++ AoC06/AoC06/FishTank.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC06
+{
+    public class FishTank
+    {
+        private Dictionary<int, long> _fishByAge = new Dictionary<int, long>
+        {
+            {0,0},
+            {1,0},
+            {2,0},
+            {3,0},
+            {4,0},
+            {5,0},
+            {6,0},
+            {7,0},
+            {8,0},
+        };
+        public long Count => _fishByAge.Sum(entry => entry.Value);
+
+        public FishTank(IEnumerable<int> fish)
+        {
+            foreach (var fishAge in fish)
+            {
+                _fishByAge[fishAge]++;
+            }
+        }
+
+        public void CalculateNextDay()
+        {
+            _fishByAge = new Dictionary<int, long>
+            {
+                {0, _fishByAge[1]},
+                {1, _fishByAge[2]},
+                {2, _fishByAge[3]},
+                {3, _fishByAge[4]},
+                {4, _fishByAge[5]},
+                {5, _fishByAge[6]},
+                {6, _fishByAge[7] + _fishByAge[0]},
+                {7, _fishByAge[8]},
+                {8, _fishByAge[0]},
+            };
+        }
+    }
+}
---
+++ AoC06/AoC06/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AoC06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = "Input.txt";
+            task1(filename);
+            task2(filename);
+        }
+
+        private static void task1(string filename)
+        {
+            Console.WriteLine("Task 1:");
+            Console.WriteLine("=======");
+
+            var fish = new FishTank(readNumbersFromFile(filename));
+            Console.WriteLine($"Day 00: {fish.Count} fish");
+            for (var day = 0; day < 80; day++)
+            {
+                fish.CalculateNextDay();
+
+                if (day % 20 == 19)
+                {
+                    Console.WriteLine($"Day {day+1}: {fish.Count} fish");
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void task2(string filename)
+        {
+            Console.WriteLine("Task 2:");
+            Console.WriteLine("=======");
+
+            var fish = new FishTank(readNumbersFromFile(filename));
+            Console.WriteLine($"Day 000: {fish.Count} fish");
+            for (var day = 0; day < 256; day++)
+            {
+                fish.CalculateNextDay();
+
+                if (day % 64 == 63)
+                {
+                    Console.WriteLine($"Day {day + 1:D3}: {fish.Count} fish");
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        private static IEnumerable<int> readNumbersFromFile(string fileName)
+        {
+            var inputFile = new FileInfo(fileName);
+            using var fileStream = inputFile.OpenRead();
+            using var fileReader = new StreamReader(fileStream);
+            var input = fileReader.ReadLine();
+            return input.Split(',').Select(int.Parse);
+        }
+    }
+}
